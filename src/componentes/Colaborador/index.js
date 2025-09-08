96618c9409628ed095cda0a4df88bc13971a0282
Gabriel Mathias <gabrielmathias526@gmail.com>
2025-09-08 17:04:35 -0300

    first

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,25 @@
+import { IoIosCloseCircle } from "react-icons/io";
+
+import "./colaborador.css";
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <IoIosCloseCircle
+        size={25}
+        className="deletar"
+        onClick={() => aoDeletar(colaborador.id)}
+      />
+
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
